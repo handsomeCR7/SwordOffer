73b40d024e89f938d3f148e730e2b5426aee0587
handsomeCR7 <270898831@qq.com>
2024-04-28 20:24:09 +0800

    18

--- 18.三.快速幂.cpp
+++ 18.三.快速幂.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     int fastPower(int a, int b, int n) {
         if (n == 0) {
-            return 1;
+            return 1 % b;
         }
         if (n == 1) {
             return a % b;
